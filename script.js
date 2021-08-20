2f3e82b8769951d74ae35b3547561b71f3c4bce5
Mitul Patel <mitul.patel@1rivet.local>
2021-08-20 16:48:58 +0530

    design

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ let to_left = document.getElementById("radio1");
 let to_right = document.getElementById("radio2");
 let to_top = document.getElementById("radio3");
 let to_bottom = document.getElementById("radio4");
-let getSelectedRadio = "";
+let to_left_text = document.getElementById("radio-text1");
+let to_right_text = document.getElementById("radio-text2");
+let to_top_text = document.getElementById("radio-text3");
+let to_bottom_text = document.getElementById("radio-text4");
+let output_color=document.getElementById("output");
+let getSelectedRadio = "to left";
 const getColor = () => {
     let color_1 = document.getElementById("color1").value;
     let color_2 = document.getElementById("color2").value;
@@ -23,7 +28,8 @@ const getColor = () => {
     // console.log(color_1);
     // console.log(color_2);
     gradient.style.background = `linear-gradient(${getSelectedRadio}, ${color_1} , ${color_2})`;
-    colorCode.innerHTML = `Code: background: linear-gradient(${getSelectedRadio}, ${color_1} , ${color_2})`;
+    colorCode.innerHTML = `background: linear-gradient(${getSelectedRadio}, ${color_1} , ${color_2});`;
+    output_color.style.background = `linear-gradient(${getSelectedRadio}, ${color_1} , ${color_2})`;
 }
 const getColorByText = () => {
     let color_text_1 = document.getElementById("color-text1").value;
@@ -36,22 +42,35 @@ const getColorByText = () => {
         alert("Enter all the colors");
     }
     else{
-    if (to_left.checked) {
+    if (to_left_text.checked) {
         getSelectedRadio = "to left";
     }
-    if (to_right.checked) {
+    if (to_right_text.checked) {
         getSelectedRadio = "to right";
     }
-    if (to_top.checked) {
+    if (to_top_text.checked) {
         getSelectedRadio = "to top";
     }
-    if (to_bottom.checked) {
+    if (to_bottom_text.checked) {
         getSelectedRadio = "to bottom";
     }
     // console.log(color_text_1);
     // console.log(color_text_2);
     gradient.style.background = `linear-gradient(${getSelectedRadio}, ${color_text_1} , ${color_text_2})`;
-    colorCode.innerHTML = `Code: background: linear-gradient(${getSelectedRadio}, ${color_text_1} , ${color_text_2})`;
+    colorCode.innerHTML = `background: linear-gradient(${getSelectedRadio}, ${color_text_1} , ${color_text_2});`;
+    output_color.style.background = `linear-gradient(${getSelectedRadio}, ${color_text_1} , ${color_text_2})`;
 }
 }
 window.onload = getColor();
+window.onload = alert("Developed by Yasin Hirani & Designed by Mitul Patel");
+
+const copyEmail = () =>{
+    const myCode = document.getElementById('code');
+    const textArea = document.createElement('textarea');
+    textArea.value = myCode.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    textArea.remove();
+    alert("Copied")
+  }
